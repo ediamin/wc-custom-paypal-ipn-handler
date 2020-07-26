c0ee1a2fb74bdb985c2f75779302ea196fdfe0f2
Edi Amin <to.ediamin@gmail.com>
2020-07-26 20:16:41 +0600

    Fix formatting

--- class-wc-gateway-paypal-ipn-handler-override.php
+++ class-wc-gateway-paypal-ipn-handler-override.php
@@ -2,14 +2,14 @@
 
 class WC_Gateway_Paypal_IPN_Handler_Override extends WC_Gateway_Paypal_IPN_Handler {
 
-	public function __construct( $sandbox = false, $receiver_email = '' ) {
-		add_action( 'valid-paypal-standard-ipn-request', array( $this, 'valid_response' ) );
+    public function __construct( $sandbox = false, $receiver_email = '' ) {
+        add_action( 'valid-paypal-standard-ipn-request', array( $this, 'valid_response' ) );
 
-		$this->receiver_email = $receiver_email;
-		$this->sandbox        = $sandbox;
+        $this->receiver_email = $receiver_email;
+        $this->sandbox        = $sandbox;
     }
 
-	protected function save_paypal_meta_data( $order, $posted ) {
+    protected function save_paypal_meta_data( $order, $posted ) {
         // Write your code here
-	}
+    }
 }
